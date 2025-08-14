ab39c7840095919e4f8d2acba36e7f03ece28987
Oliver Fu <ofu997@gmail.com>
2025-08-14 16:54:57 -0700

    save current progress on styling, responsive About section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,6 @@ import { Fade } from 'react-awesome-reveal';
 import aboutText from '../assets/text';
 import { curriculumVitae } from '../assets/links';
 import Picture from '../assets/nathanNewAboutMe.jpg';
-import { Image } from 'astro:assets';
 
 const About = () => {
   const [isDesktop, setIsDesktop] = useState(false);
@@ -21,28 +20,30 @@ const About = () => {
   }, []);
 
   return (
-    <section id="about" style={{ backgroundColor: '#082567', maxWidth: '99%'}} className='jumbotron'>
+    <section id="about" style={{ backgroundColor: '#082567' }} 
+    // className='jumbotron'
+    >
       <div>
         <Fade delay={1e3}>
             <h2 className="section-title">About Me</h2>
         </Fade>
-        <div className='aboutMeImageAndDescription' 
+        <div className='aboutMeImageAndDescription' id="aboutSectionParentDiv" 
           style={{ 
             display: 'flex',
              margin: 'auto', 
-             justifyContent: 'center' 
+             justifyContent: 'center', 
           }}
         >
           <div className='about-wrapper__image'>
-            <Image src={Picture.src} alt="Nathan Ellstrand's profile picture" />
+            <img src={Picture.src} alt="Nathan Ellstrand's profile picture" />
           </div>
-          <Fade delay={1e3}>
+          {/* <Fade delay={1e3}> */}
             <div className="about-wrapper__info" >
               <p className="about-wrapper__info-text" style={{ textAlign: 'left', margin: '0 5%', whiteSpace: 'pre-line'}}>
                 {aboutText}
               </p>
             </div>
-          </Fade>
+          {/* </Fade> */}
         </div>
         <div style={{height: 100 }}></div>
         <p className='about-cta'>
